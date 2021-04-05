2b8585e4dcd6a00a4b7e795e571f6482c183846e
Pramod Sheoran <pramodsheoran599@gmail.com>
2021-04-05 22:58:21 +0530

    Test Code Update for centroid in Detection.py

--- Test Code/Detection.py
+++ Test Code/Detection.py
@@ -1,36 +1,51 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('..\\Videos\\Abuse001_x264.mp4')
-fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=60, detectShadows=False)
+cap = cv2.VideoCapture('Videos\\pedestrians.avi')                                                   # Loading the Video
+fgbg = cv2.createBackgroundSubtractorKNN(history=100)                                                         # Background Subtractor
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))                                       # Kernel
 
 while cap.isOpened():
-    ret, frame = cap.read()
-    if frame is None:
-        break
+    ret, frame = cap.read()                                                                         # Read the Frame from Video
+    if frame is None:                                                                               # If Last frame is read
+        break                                                                                            # Exit Loop
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (3, 3), 0)
-    fgmask = fgbg.apply(blur)
+    blank_image = np.zeros((frame.shape[:2]), np.uint8)                                             # Creating a Black Image of Same Size
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                                                  # Gray Conversion
+    blur = cv2.GaussianBlur(gray, (5, 5), 2)                                                        # Applying Blur
+    fgmask = fgbg.apply(blur)                                                                       # Creating Foreground Mask
 
-    dilated = cv2.dilate(fgmask, (3, 3), iterations=1)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    diff = blank_image - fgmask                                                                     # Difference Between Mask and Blank Image
+    # dilated = cv2.dilate(diff, (7, 7), iterations=2)                                                # Dilating the Difference
+
+    # diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, (3, 3), iterations=3)                      # Opening ( False Positives Correction )
+    # diff = cv2.morphologyEx(diff, cv2.MORPH_CROSS, (3, 3), iterations=3)                     # Closing ( False Negatives Correction )
+
+    dilated = cv2.dilate(diff, (3, 3))
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)                 # Finding Contours
+
+    persons = []                                                                                    # List of Detected People
 
     for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
+        (x, y, w, h) = cv2.boundingRect(contour)                                                    # Coordinates of the Contour
 
-        if cv2.contourArea(contour) < 80:
-            continue
+        if cv2.contourArea(contour) < 480:                                                          # If Contour Less than Threshold
+            continue                                                                                    # Skip the Contour
 
-        # x = roi.shape[:]
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        persons.append((x, y, w, h))                                                                # Add the Coordinate to Person List
 
-    # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
-    cv2.imshow("Frame", frame)
-    cv2.imshow("Dilated", dilated)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)                                # Draw the Bounding Box
 
-    if cv2.waitKey(40) == 27:
-        break
+    for person in persons:                                                                          # For each person
+        x, y, w, h = person                                                                             # Coordinates Unpacking
+        centroid = ((x + w//2), (y + h//2))                                                             # Centroid
+        cv2.circle(frame, centroid, 2, (0, 0, 255), 2)                                                  # Draw Centroid
+
+    cv2.imshow("Frame", frame)                                                                      # Display Orignal Frame
+    cv2.imshow("Dilated", dilated)                                                                  # Display Dilated Frame
+
+    if cv2.waitKey(60) == 27:                                                                       # If Esc Key Pressed
+        break                                                                                           # Exit the Loop
 
 cv2.destroyAllWindows()
 cap.release()
